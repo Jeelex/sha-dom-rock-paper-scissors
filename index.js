4b0bffaf01ddb39a83ebcc58a84563086d04cb4d
Gogi <gogion@protonmail.com>
2021-02-26 00:59:37 +0200

    completed dom-rock-paper-scissors

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const btnScissors = document.getElementById('scissors');
 const playerScoreNumber = document.getElementById('playerScoreNumber');
 const computerScoreNumber = document.getElementById('computerScoreNumber');
 const roundScore = document.getElementById('roundScore');
-
-
+const winner = document.getElementById('winner');
 
 let result;
 let playerScore = 0;
@@ -15,104 +14,70 @@ let computerScore = 0;
 let playerScoreTotal = 0;
 let computerScoreTotal = 0;
 
-
+// ********					  Button Event Listeners
 btnRock.addEventListener('click', () => {
 	playRound('rock', computerSelection);
 	playerScoreNumber.textContent = playerScoreTotal;
 	computerScoreNumber.textContent = computerScoreTotal;
-	// if (playerScoreTotal)
 	checkWinner(playerScoreTotal, computerScoreTotal);
 });
 
 btnPaper.addEventListener('click', () => {
 	playRound('paper', computerSelection);
-	
+	playerScoreNumber.textContent = playerScoreTotal;
+	computerScoreNumber.textContent = computerScoreTotal;
+	checkWinner(playerScoreTotal, computerScoreTotal);
 });
 
 btnScissors.addEventListener('click', () => {
 	playRound('scissors', computerSelection);
-	
+	playerScoreNumber.textContent = playerScoreTotal;
+	computerScoreNumber.textContent = computerScoreTotal;
+	checkWinner(playerScoreTotal, computerScoreTotal);
 });
 
-function game() {
-	// *                     5 round game using for loop
-	// for (let i = 0; i < 5; i++) {
-	// 	playRound(playerSelection, computerSelection);
-	// }
-
-  playRound(playerSelection, computerSelection);   // * playing only 1 round until I create the UI
-
-	// !                   colors in console currently working ONLY in Google Chrome
-	console.log(
-		'\x1b[35m%s\x1b[0m',
-		`Player Total: ${playerScoreTotal}, Computer Total: ${computerScoreTotal}`
-	);
-
-	return checkWinner(playerScoreTotal, computerScoreTotal);
-}
-// game();
-
+// TODO		 This is better written than the previous version in the original RPS repo. Need to update it.
 function checkWinner(player1Score, player2Score) {
-	if (player1Score > player2Score) {
-		console.log('\x1b[42m%s\x1b[0m', "You're the WINNER!");
-	} else if (player1Score === player2Score) {
-		console.log('\x1b[33m%s\x1b[0m', "It's a DRAW!");
-	} else {
-		console.log('\x1b[31m%s\x1b[0m', 'You LOSE!');
+	if (player1Score >= 5) {
+		winner.textContent = "You're the WINNER!";
+	} else if (player2Score >= 5) {
+		winner.textContent = 'Computer wins! You LOSE!';
 	}
 }
-
-// *   					keeping this as backup!
-// function checkWinner(player1Score, player2Score) {
-// 	if (player1Score > player2Score) {
-// 		console.log('\x1b[42m%s\x1b[0m', "You're the WINNER!");
-// 	} else if (player1Score === player2Score) {
-// 		console.log('\x1b[33m%s\x1b[0m', "It's a DRAW!");
-// 	} else {
-// 		console.log('\x1b[31m%s\x1b[0m', 'You LOSE!');
-// 	}
-// }
 
 function playRound(playerSelection, computerSelection) {
 	playerScore = 0;
 	computerScore = 0;
 
 	computerSelection = computerPlay();
-	// playerSelection = prompt(
-	// 	'PLEASE TYPE:  rock, paper or scissors'
-	// ).toLowerCase();
-	console.log(`Player: ${playerSelection}`);
-	console.log(`Computer: ${computerSelection}`);
-
-	// TODO            might be better to turn the if statement below into a switch statement???
 
 	if (playerSelection === 'rock' && computerSelection === 'paper') {
 		computerScore++;
 		result = 'You Lose!';
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (playerSelection === 'paper' && computerSelection === 'paper') {
 		playerScore++;
 		computerScore++;
 		result = "It's a draw!";
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (
 		playerSelection === 'scissors' &&
 		computerSelection === 'paper'
 	) {
 		playerScore++;
 		result = 'You Win!';
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
 		playerScore++;
 		result = 'You Win!';
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (
 		playerSelection === 'paper' &&
 		computerSelection === 'scissors'
 	) {
 		computerScore++;
 		result = 'You Lose!';
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (
 		playerSelection === 'scissors' &&
 		computerSelection === 'scissors'
@@ -120,22 +85,20 @@ function playRound(playerSelection, computerSelection) {
 		playerScore++;
 		computerScore++;
 		result = "It's a draw!";
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (playerSelection === 'rock' && computerSelection === 'rock') {
 		playerScore++;
 		computerScore++;
 		result = "It's a draw!";
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
 		playerScore++;
 		result = 'You Win!';
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
 		computerScore++;
 		result = 'You Lose!';
-		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore} -> ${result}`;
-	} else {
-		roundScore.textContent = "Sorry! You can only choose between 'rock', 'paper' or 'scissors' !";
+		roundScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
 	}
 
 	playerScoreTotal += playerScore;
